4636be68209ab60220fcd48353caa313c22b9192
yann dupis <yann.dupis@gmail.com>
2020-03-24 14:43:51 -0700

    add test

--- tensorflow_federated/python/core/impl/executors/secure_client_test.py
+++ tensorflow_federated/python/core/impl/executors/secure_client_test.py
@@ -153,6 +153,27 @@ class SecureClientTest(parameterized.TestCase):
     result = _run_test_comp_produces_federated_value(self, comp, num_clients=3)
     self.assertEqual(result.numpy(), 60)
 
+  def test_federated_zip_secure_client_values_v2(self):
+    @computations.tf_computation(tf.int32, tf.int32)
+    def add_numbers(x, y):
+      return x + y
+
+    @computations.tf_computation(tf.int32, tf.int32)
+    def encrypt_tensor(x, y):
+      return tf.add(x, y)
+
+    @computations.federated_computation
+    def comp():
+      return intrinsics.federated_reduce(
+        intrinsics.federated_map(encrypt_tensor,
+          intrinsics.federated_zip(
+            [intrinsics.federated_value(10, placements.CLIENTS), 
+            intrinsics.federated_value(10, placements.CLIENTS)])),
+          0, add_numbers)
+
+    result = _run_test_comp_produces_federated_value(self, comp, num_clients=3)
+    self.assertEqual(result.numpy(), 60)
+
   
 if __name__ == '__main__':
   absltest.main()
